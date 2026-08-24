aa57431c296543af435df128cfed59b0630f3e3d
Ferri Dharma <nagari.lambah@gmail.com>
2026-08-24 21:51:49 +0700

    Fix Supabase production key

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,11 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://hlphkhptivkovobqysby.supabase.co";
-
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
-  "sb_publishable_Am68Hm3NtAJyNDI7-FGlBg_6C7Ayo3B";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
