437e28de55a891a6591a619f86ede552f1ce7115
Ferri Dharma <nagari.lambah@gmail.com>
2026-08-24 22:37:33 +0700

    Perbaiki Supabase client dan endpoint cek

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseUrl =
+  "https://hlphkhptivkovobqysby.supabase.co";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseKey =
+  "sb_publishable_Am68Hm3NtAJyNDI7-FGlBg_6C7Ayo3B";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
